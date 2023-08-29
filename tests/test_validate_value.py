33f51aaab7b4e5fc31f421ff821c73faccd97938
Ståle Storø Hauknes <stale.semb@airthings.com>
2023-08-29 23:36:41 +0200

    Adjust tests

--- tests/test_validate_value.py
+++ tests/test_validate_value.py
@@ -1,16 +1,15 @@
 import pytest
 
 from airthings_ble.parser import validate_value
-from airthings_ble.const import HUMIDITY_MAX as max
+from airthings_ble.const import HUMIDITY_MAX, RADON_MAX
 
+min = 0.0
 
-def test_validate_value():
-    min = 0.0
-
+def test_validate_value_humidity():
     valid_humidity_values = [0, 50, 100.0]
     for value in valid_humidity_values:
-        assert validate_value(value=value, min=min, max=max) == value
+        assert validate_value(value=value, min=min, max=HUMIDITY_MAX) == value
 
     invalid_humidity_values = [-1, 100.1, 101]
     for value in invalid_humidity_values:
-        assert validate_value(value=value, min=min, max=max) is None
+        assert validate_value(value=value, min=min, max=HUMIDITY_MAX) is None
