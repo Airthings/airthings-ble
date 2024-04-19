9c83d48201fea83bf1c9c8c8450adb3160b35627
Ståle Storø Hauknes <stale.semb@airthings.com>
2024-04-19 15:10:18 +0200

    Rename battery functions

--- airthings_ble/device_type.py
+++ airthings_ble/device_type.py
@@ -50,37 +50,37 @@ class AirthingsDeviceType(Enum):
     def battery_percentage(self, voltage: float) -> int:
         """Calculate battery percentage based on voltage."""
         if self == AirthingsDeviceType.WAVE_MINI:
-            return round(self._battery_percentage_wave_mini(voltage))
-        return round(self._battery_percentage_wave_radon_and_plus(voltage))
+            return round(self._two_batteries(voltage))
+        return round(self._three_batteries(voltage))
 
     # pylint: disable=too-many-return-statements
-    def _battery_percentage_wave_radon_and_plus(self, voltage: float) -> float:
+    def _two_batteries(self, voltage: float) -> float:
         if voltage >= 3.00:
             return 100
         if 2.80 <= voltage < 3.00:
             return self._interpolate(
-                voltage, voltage_range=(2.80, 3.00), percentage_range=(81, 100)
+                voltage=voltage, voltage_range=(2.80, 3.00), percentage_range=(81, 100)
             )
         if 2.60 <= voltage < 2.80:
             return self._interpolate(
-                voltage, voltage_range=(2.60, 2.80), percentage_range=(53, 81)
+                voltage=voltage, voltage_range=(2.60, 2.80), percentage_range=(53, 81)
             )
         if 2.50 <= voltage < 2.60:
             return self._interpolate(
-                voltage, voltage_range=(2.50, 2.60), percentage_range=(28, 53)
+                voltage=voltage, voltage_range=(2.50, 2.60), percentage_range=(28, 53)
             )
         if 2.20 <= voltage < 2.50:
             return self._interpolate(
-                voltage, voltage_range=(2.20, 2.50), percentage_range=(5, 28)
+                voltage=voltage, voltage_range=(2.20, 2.50), percentage_range=(5, 28)
             )
         if 2.10 <= voltage < 2.20:
             return self._interpolate(
-                voltage, voltage_range=(2.10, 2.20), percentage_range=(0, 5)
+                voltage=voltage, voltage_range=(2.10, 2.20), percentage_range=(0, 5)
             )
         return 0
 
     # pylint: disable=too-many-return-statements
-    def _battery_percentage_wave_mini(self, voltage: float) -> float:
+    def _three_batteries(self, voltage: float) -> float:
         if voltage >= 4.50:
             return 100
         if 4.20 <= voltage < 4.50:
