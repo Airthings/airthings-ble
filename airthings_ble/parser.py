76e9c6ef26c43de2dba13ebf0b06699888d6479f
Ståle Storø Hauknes <stale.semb@airthings.com>
2025-01-01 23:21:57 +0100

    Linting bugfix

--- airthings_ble/parser.py
+++ airthings_ble/parser.py
@@ -848,7 +848,7 @@ class AirthingsBluetoothDeviceData:
         disconnect_future = loop.create_future()
         client: BleakClientWithServiceCache = (
             await establish_connection(  # pylint: disable=line-too-long
-                BleakClientWithServiceCache,  # type: ignore
+                BleakClientWithServiceCache,
                 ble_device,
                 ble_device.address,
                 disconnected_callback=partial(
