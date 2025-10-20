e35de72f05681f6d5996f84fbb4b055fcceb6848
Ståle Storø Hauknes <stale.semb@airthings.com>
2025-10-20 13:22:06 +0200

    Update mini tests

--- tests/test_wave_mini.py
+++ tests/test_wave_mini.py
@@ -1,13 +1,13 @@
 import logging
 
-from airthings_ble.parser import WaveRadonAndPlusCommandDecode, _decode_wave_plus
+from airthings_ble.parser import WaveMiniCommandDecode, _decode_wave_mini
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def test_wave_plus_command_decode() -> None:
+def test_wave_mini_command_decode() -> None:
     """Test wave plus command decode."""
-    decode = WaveRadonAndPlusCommandDecode()
+    decode = WaveMiniCommandDecode()
     assert decode.decode_data(
         logger=_LOGGER,
         raw_data=bytearray.fromhex(
@@ -16,19 +16,15 @@ def test_wave_plus_command_decode() -> None:
     ) == {"battery": 3.0}
 
 
-def test_wave_plus_sensor_data() -> None:
-    """Test wave plus sensor data."""
+def test_wave_mini_sensor_data() -> None:
+    """Test wave mini sensor data."""
     raw_data = bytearray.fromhex("01380d800b002200bd094cc31d036c0000007d05")
 
-    decoded_data = _decode_wave_plus(name="Plus", format_type="<4B8H", scale=1.0)(
+    decoded_data = _decode_wave_mini(name="Mini", format_type="<4B8H", scale=1.0)(
         raw_data
     )
 
     assert decoded_data["humidity"] == 28.0
-    assert decoded_data["radon_1day_avg"] == 11
-    assert decoded_data["radon_longterm_avg"] == 34
     assert decoded_data["temperature"] == 24.93
     assert decoded_data["voc"] == 108
-    assert decoded_data["co2"] == 797
-    assert decoded_data["illuminance"] == 5
     assert decoded_data["pressure"] == 999.92
