d90b56121fa6f8a4ad3fc8c6f44200c3dc41051d
Ståle Storø Hauknes <stale.semb@airthings.com>
2025-11-02 22:45:52 +0100

    Update test

--- tests/test_wave_mini.py
+++ tests/test_wave_mini.py
@@ -1,36 +1,38 @@
 import logging
 
-from airthings_ble.command_decode import WaveRadonAndPlusCommandDecode
-from airthings_ble.const import BATTERY
-from airthings_ble.sensor_decoders import _decode_wave_plus
+from airthings_ble.command_decode import WaveMiniCommandDecode
+from airthings_ble.const import (
+    BATTERY,
+    HUMIDITY,
+    ILLUMINANCE,
+    PRESSURE,
+    TEMPERATURE,
+    VOC,
+)
+from airthings_ble.sensor_decoders import _decode_wave_mini
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def test_wave_plus_command_decode() -> None:
-    """Test wave plus command decode."""
-    decode = WaveRadonAndPlusCommandDecode()
+def test_wave_mini_command_decode() -> None:
+    """Test Wave Mini command decode for battery."""
+    decode = WaveMiniCommandDecode()
     assert decode.decode_data(
         logger=_LOGGER,
         raw_data=bytearray.fromhex(
-            "6d00600c04000100008211ff00000000c04c20001f3560007006B80B0900"
+            "6d0064000000c800000001020304f4015802bc02000020038403b80b4c04b0040000"
         ),
     ) == {BATTERY: 3.0}
 
 
-def test_wave_plus_sensor_data() -> None:
-    """Test wave plus sensor data."""
-    raw_data = bytearray.fromhex("01380d800b002200bd094cc31d036c0000007d05")
-
-    decoded_data = _decode_wave_plus(name="Plus", format_type="<4B8H", scale=1.0)(
-        raw_data
+def test_wave_mini_sensor_data() -> None:
+    """Test Wave Mini sensor data decoding."""
+    decoded_data = _decode_wave_mini(name="WaveMini", format_type="<2B5HLL", scale=1.0)(
+        bytearray.fromhex("1800327431c168102e000000ff940700ffffffff")
     )
 
-    assert decoded_data["humidity"] == 28.0
-    assert decoded_data["radon_1day_avg"] == 11
-    assert decoded_data["radon_longterm_avg"] == 34
-    assert decoded_data["temperature"] == 24.93
-    assert decoded_data["voc"] == 108
-    assert decoded_data["co2"] == 797
-    assert decoded_data["illuminance"] == 5
-    assert decoded_data["pressure"] == 999.92
+    assert decoded_data[ILLUMINANCE] == 9.0
+    assert decoded_data[TEMPERATURE] == 24.31
+    assert decoded_data[PRESSURE] == 989.14
+    assert decoded_data[HUMIDITY] == 42.0
+    assert decoded_data[VOC] == 46.0
